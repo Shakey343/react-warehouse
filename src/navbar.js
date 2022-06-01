9a73541e14a31de308387e8a04225e97f68b1b04
Jake Pople <jakepople@hotmail.co.uk>
2022-06-01 16:40:02 +0100

    sass added

---
+++ src/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Navbar = () => {
+  return (
+    <div>
+      <h1>The Warehouse - Waterloo</h1>
+      <ul>
+        <li>Home</li>
+        <li>Studios</li>
+        <li>Contact</li>
+        <li>Shop</li>
+      </ul>
+    </div>
+  );
+}
